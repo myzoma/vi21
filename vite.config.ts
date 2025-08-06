236e1ba9d794eda31db2f335191faa6afe59baf6
myzoma <86922707+myzoma@users.noreply.github.com>
2025-08-06 19:16:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/vi21/', // اسم المشروع على GitHub Pages
+  base: '/vi21/',
 
   plugins: [
     react(),
@@ -41,3 +41,4 @@ export default defineConfig({
     },
   },
 });
+
